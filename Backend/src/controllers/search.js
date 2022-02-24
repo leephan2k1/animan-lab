9ebd0584e22c97c9e99e356dd2723a1ef440446a
lee phan <pttl2k1@gmail.com>
2022-02-24 15:15:56 +0700

    sort filter

--- Backend/src/controllers/search.js
+++ Backend/src/controllers/search.js
@@ -3,28 +3,52 @@ const Post = require("../models/Post");
 module.exports = {
   filters: async (req, res, next) => {
     const filters = req.query;
-    const { title, tags } = filters;
+    const { title, tags, sort } = filters;
+    let posts;
 
+    //title filter
     if (title) {
-      const posts = await Post.find(
+      posts = await Post.find(
         {
           title: { $regex: title, $options: "i" },
         },
         { __v: 0 }
       );
-      return res.status(200).json({ success: true, posts });
     }
 
+    //sort filter
+    if (sort) {
+      if (!posts) {
+        posts = await Post.find({}, { _v: 0 });
+      }
+      switch (sort) {
+        case "asc":
+          posts = posts.sort(
+            (a, b) =>
+              new Date(a.createdAt.toString()) -
+              new Date(b.createdAt.toString())
+          );
+          break;
+        case "desc":
+          posts = posts.sort(
+            (a, b) =>
+              new Date(b.createdAt.toString()) -
+              new Date(a.createdAt.toString())
+          );
+          break;
+      }
+    }
+
+    //tags filter
     if (tags) {
-      const posts = await Post.find(
+      posts = await Post.find(
         {
           tags: { $in: [tags] },
         },
         { __v: 0 }
       );
-      return res.status(200).json({ success: true, posts });
     }
 
-    return res.status(404).json({ success: false, message: "Not found" });
+    return res.status(200).json({ success: true, posts });
   },
 };
