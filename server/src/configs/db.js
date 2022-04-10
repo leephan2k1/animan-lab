345e200482f68e22c06feb611d9e83cec533abec
lee phan <pttl2k1@gmail.com>
2022-04-10 18:19:16 +0700

    config connect mongo

--- server/src/configs/db.js
+++ server/src/configs/db.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 async function connect() {
   try {
-    await mongoose.connect("mongodb://localhost:27017/animan-lab");
+    await mongoose.connect("mongodb://127.0.0.1:27017/animan-lab");
     console.log("connect success!");
   } catch (e) {
     console.log("connect failed: " + e.message);
